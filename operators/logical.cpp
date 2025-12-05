26f90f02546bb3e0e4e4b3ba7f82b180298f315f
AIChoubeyX <AIChoubeyX@users.noreply.github.com>
2025-12-05 14:10:45 +0530

    operators completed

---
+++ operators/logical.cpp
@@ -0,0 +1,29 @@
+# include<iostream>
+using namespace std;
+
+int main (){
+    // int x,y,z;
+    // cout<<"Enter three numbers: ";
+    // cin>>x>>y>>z;
+    // if(x > 0 && y > 0){
+    //     cout<<"Both numbers are positive."<<endl;
+    // }   
+    // if(x > 0 || y > 0){
+    //     cout<<"At least one number is positive."<<endl;
+    // }
+    // if(!(x > 0)){
+    //     cout<<"The first number is not positive."<<endl;
+    // }
+    // if (x>y && x>z){
+    //     cout  << "x is the largest number." << endl;
+
+    // }
+
+    char name = 'c';
+    if (name == 'a' || name == 'e' || name == 'i' || name == 'o' || name == 'u'){
+        cout << name << " is a vowel." << endl;
+    }
+    else {
+        cout << name << " is a consonant." << endl;
+    }
+}
